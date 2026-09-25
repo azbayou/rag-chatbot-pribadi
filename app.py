62645a13a310d429b9458f079ec408f4399f04ad
azbayou <azbayou@gmail.com>
2026-09-25 19:22:39 +0700

    commit

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from langchain_core.output_parsers import StrOutputParser
 
 # 1. Konfigurasi Halaman Streamlit
 st.set_page_config(page_title="RAG Chatbot Asisten Pribadi", page_icon="🤖")
-st.title("🤖 Chatbot Asisten Miyo")
+st.title("🐱 Chatbot Asisten Miyo")
 st.caption("Tanyakan mengenai CV/Catatan Bayu, atau perkembangan seputar Data & AI!")
 
 # 2. Ambil API Key dari Streamlit Secrets
