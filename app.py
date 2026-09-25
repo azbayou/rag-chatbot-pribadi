a1dccd887057fe2dfcd7f4dac0295c35b48d0fc7
azbayou <azbayou@gmail.com>
2026-09-25 19:26:36 +0700

    commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 
 # 1. Konfigurasi Halaman Streamlit
-st.set_page_config(page_title="RAG Chatbot Asisten Pribadi", page_icon="🤖")
+st.set_page_config(page_title="Miyo Chatbot Asisten Pribadi", page_icon="🐱")
 st.title("🐱 Chatbot Asisten Miyo")
 st.caption("Tanyakan mengenai CV/Catatan Bayu, atau perkembangan seputar Data & AI!")
 
